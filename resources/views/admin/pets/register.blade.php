79ecf8650d857305d213ee25aa3a5c6e7117d307
Gustavo Hernandez Hidalgo <tottiav2@gmail.com>
2021-10-11 18:28:15 -0500

    design admin and import new components Vue

---
+++ resources/views/admin/pets/register.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.adminApp')
+
+@section('content')
+    <div id="app" name="app" class="app">
+        <home-admin />
+    </div>
+@endsection
+
+@section('scripts')
+
+@endsection
