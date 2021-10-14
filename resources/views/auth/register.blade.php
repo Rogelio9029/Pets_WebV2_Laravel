9564b0abdde22e0ae88fa3ebff7db136239a9285
halolo1 <chavamaster123@gmail.com>
2021-10-14 16:35:33 -0500

    desing interfaces landding for users

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,7 +1,7 @@
-@extends('layouts.app')
+@extends('layouts.nav_user')
 
 @section('content')
-    <div class="min-w-screen md:min-h-screen flex items-center justify-center px-5 py-5">
+    <div class="min-w-screen md:min-h-screen flex items-center justify-center px-2 py-2">
         <div class="bg-white text-gray-500 rounded-xl shadow-2xl w-full overflow-hidden" style="max-width:1000px">
             <div class="md:flex w-full">
                 <img src="{{ asset('images/SignUp.jpg') }}" resizeMode = 'cover'  class="hidden md:block w-1/2" srcset="">
@@ -13,14 +13,14 @@
                     <div>
                         <div class="flex -mx-3">
                             <div class="w-1/2 px-3 mb-5">
-                                <label for="" class="text-xs font-semibold px-1">First name</label>
+                                <label for="" class="text-xs font-semibold px-1">Nombre</label>
                                 <div class="flex">
                                     <div class="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"><i class="mdi mdi-account-outline text-gray-400 text-lg"></i></div>
                                     <input type="text" class="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500" placeholder="John">
                                 </div>
                             </div>
                             <div class="w-1/2 px-3 mb-5">
-                                <label for="" class="text-xs font-semibold px-1">Last name</label>
+                                <label for="" class="text-xs font-semibold px-1">Apellido</label>
                                 <div class="flex">
                                     <div class="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"><i class="mdi mdi-account-outline text-gray-400 text-lg"></i></div>
                                     <input type="text" class="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500" placeholder="Smith">
@@ -29,7 +29,7 @@
                         </div>
                         <div class="flex -mx-3">
                             <div class="w-full px-3 mb-5">
-                                <label for="" class="text-xs font-semibold px-1">Email</label>
+                                <label for="" class="text-xs font-semibold px-1">Correo electronico</label>
                                 <div class="flex">
                                     <div class="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"><i class="mdi mdi-email-outline text-gray-400 text-lg"></i></div>
                                     <input type="email" class="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500" placeholder="johnsmith@example.com">
@@ -38,7 +38,7 @@
                         </div>
                         <div class="flex -mx-3">
                             <div class="w-full px-3 mb-12">
-                                <label for="" class="text-xs font-semibold px-1">Password</label>
+                                <label for="" class="text-xs font-semibold px-1">Contraseña</label>
                                 <div class="flex">
                                     <div class="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"><i class="mdi mdi-lock-outline text-gray-400 text-lg"></i></div>
                                     <input type="password" class="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500" placeholder="************">
@@ -47,7 +47,7 @@
                         </div>
                         <div class="flex -mx-3">
                             <div class="w-full px-3 mb-5">
-                                <button class="block w-full max-w-xs mx-auto bg-indigo-500 hover:bg-indigo-700 focus:bg-indigo-700 text-white rounded-lg px-3 py-3 font-semibold">REGISTER NOW</button>
+                                <button class="block w-full max-w-xs mx-auto bg-indigo-500 hover:bg-indigo-700 focus:bg-indigo-700 text-white rounded-lg px-3 py-3 font-semibold">REGISTRAR</button>
                             </div>
                         </div>
                     </div>
--- routes/web.php
+++ routes/web.php
@@ -13,13 +13,36 @@
 
 Route::get('/', function () {
     // return view('User_Views/Home');
-    return view('welcome');
+    return view('Home');
 })->name('/');
 
-Route::get('index', function () {
+Route::get('services', function () {
     // return view('User_Views/Home');
-    return view('index');
-})->name('index');
+    return view('Services');
+})->name('services');
+
+Route::get('contact', function () {
+    // return view('User_Views/Home');
+    return view('Contact');
+})->name('contact');
+
+Route::get('about', function () {
+    // return view('User_Views/Home');
+    return view('About');
+})->name('about');
+
+Route::get('login', function () {
+    // return view('User_Views/Home');
+    return view('auth/login');
+})->name('login');
+
+Route::get('register', function () {
+    // return view('User_Views/Home');
+    return view('auth/register');
+})->name('register');
+
+
+
 
 Auth::routes();
 
