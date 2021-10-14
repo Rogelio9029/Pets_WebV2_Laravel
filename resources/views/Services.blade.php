9e887836776fc18d438d06db2d9c42f10c691798
halolo1 <chavamaster123@gmail.com>
2021-10-14 16:38:49 -0500

    update changes landding

---
+++ resources/views/Services.blade.php
@@ -0,0 +1,169 @@
+@extends('layouts.nav_user')
+
+@section('content')
+
+<div class="heading text-center font-bold text-2xl m-5 text-black">SERVICIOS</div>
+<div class="heading text-center font-normal text-2xl m-8 text-black mt-2 no-underline">Contamos con una variedad de servicios que seran de agrado tanto para el dueño de la mascota como para la mascota.</div>
+
+
+<div class="holder mx-auto w-11/12 grid sm:grid-cols-1 md:grid-cols-2 2xl:grid-cols-4 justify-center">
+
+  <div class="min-h-full mb-8 flex items-start mx-4 shadow-xl">
+    <div class="container mx-auto p-9 max-w-sm rounded-2xl overflow-hidden shadow-xl hover:shadow-2xl transition duration-300">
+      <img class="rounded-xl" src="https://images.unsplash.com/photo-1547517023-7ca0c162f816" alt="" />
+      <div class="flex justify-between items-center">
+        <div>
+          <h1 class="mt-5 mr-3 text-1xl font-semibold">Aloe Cactus</h1>
+          <p class="mt-2">$11.99</p>
+        </div>
+        <div>
+          <button class="text-white text-md font-semibold bg-yellow-500 py-2 px-4 rounded-lg shadow-md hover:shadow-lg transition duration-500 transform-gpu hover:scale-110 ">SOLICITAR</button>
+        </div>
+      </div>
+      <div class="overflow-hidden">
+         <span class="text-lg">Decripcion</span>
+         <p>jnd vcwnucv vwnuo cujwqhnd vwuodjhc vnsuodhnc cv xjonwc xcvnwjdnv xijhbcq</p>
+      </div>
+    </div>
+  </div>
+  
+  <div class="min-h-full mb-8  flex items-start mx-4 shadow-xl">
+    <div class="container mx-auto p-9 max-w-sm rounded-2xl overflow-hidden shadow-xl hover:shadow-2xl transition duration-300">
+      <img class="rounded-xl" src="https://images.unsplash.com/photo-1547517023-7ca0c162f816" alt="" />
+      <div class="flex justify-between items-center">
+        <div>
+          <h1 class="mt-5 mr-3 text-1xl font-semibold">Aloe Cactus</h1>
+          <p class="mt-2">$11.99</p>
+        </div>
+        <div>
+          <button class="text-white text-md font-semibold bg-yellow-500 py-2 px-4 rounded-lg shadow-md hover:shadow-lg transition duration-500 transform-gpu hover:scale-110 ">SOLICITAR</button>
+        </div>
+      </div>
+      <div>
+         <span class="text-lg">Decripcion</span>
+         <p>jnd vcwnucv vwnuo cujwqhnd vwuodjhc vnsuodhnc cv xjonwc xcvnwjdnv xijhbcq</p>
+      </div>
+    </div>
+  </div>
+  
+  <div class="min-h-full mb-8 flex items-start mx-4 shadow-xl">
+    <div class="container mx-auto p-9 max-w-sm rounded-2xl overflow-hidden shadow-xl hover:shadow-2xl transition duration-300">
+      <img class="rounded-xl" src="https://images.unsplash.com/photo-1547517023-7ca0c162f816" alt="" />
+      <div class="flex justify-between items-center">
+        <div>
+          <h1 class="mt-5 mr-3 text-1xl font-semibold">Aloe Cactus</h1>
+          <p class="mt-2">$11.99</p>
+        </div>
+        <div>
+          <button class="text-white text-md font-semibold bg-yellow-500 py-2 px-4 rounded-lg shadow-md hover:shadow-lg transition duration-500 transform-gpu hover:scale-110 ">SOLICITAR</button>
+        </div>
+      </div>
+      <div>
+         <span class="text-lg">Decripcion</span>
+         <p>jnd vcwnucv vwnuo cujwqhnd vwuodjhc vnsuodhnc cv xjonwc xcvnwjdnv xijhbcq</p>
+      </div>
+    </div>
+  </div>
+  
+  <div class="min-h-full mb-8 flex items-start mx-4 shadow-xl">
+    <div class="container mx-auto p-9 max-w-sm rounded-2xl overflow-hidden shadow-xl hover:shadow-2xl transition duration-300">
+      <img class="rounded-xl" src="https://images.unsplash.com/photo-1547517023-7ca0c162f816" alt="" />
+      <div class="flex justify-between items-center">
+        <div>
+          <h1 class="mt-5 mr-3 text-1xl font-semibold">Aloe Cactus</h1>
+          <p class="mt-2">$11.99</p>
+        </div>
+        <div>
+          <button class="text-white text-md font-semibold bg-yellow-500 py-2 px-4 rounded-lg shadow-md hover:shadow-lg transition duration-500 transform-gpu hover:scale-110 ">SOLICITAR</button>
+        </div>
+      </div>
+      <div>
+         <span class="text-lg">Decripcion</span>
+         <p>jnd vcwnucv vwnuo cujwqhnd vwuodjhc vnsuodhnc cv xjonwc xcvnwjdnv xijhbcq</p>
+      </div>
+    </div>
+  </div>
+  <div class="min-h-full mb-8 flex items-start mx-4 shadow-xl">
+    <div class="container mx-auto p-9 max-w-sm rounded-2xl overflow-hidden shadow-xl hover:shadow-2xl transition duration-300">
+      <img class="rounded-xl" src="https://images.unsplash.com/photo-1547517023-7ca0c162f816" alt="" />
+      <div class="flex justify-between items-center">
+        <div>
+          <h1 class="mt-5 mr-3 text-1xl font-semibold">Aloe Cactus</h1>
+          <p class="mt-2">$11.99</p>
+        </div>
+        <div>
+          <button class="text-white text-md font-semibold bg-yellow-500 py-2 px-4 rounded-lg shadow-md hover:shadow-lg transition duration-500 transform-gpu hover:scale-110 ">SOLICITAR</button>
+        </div>
+      </div>
+      <div>
+         <span class="text-lg">Decripcion</span>
+         <p>jnd vcwnucv vwnuo cujwqhnd vwuodjhc vnsuodhnc cv xjonwc xcvnwjdnv xijhbcq</p>
+      </div>
+    </div>
+  </div>
+  
+  <div class="min-h-full mb-8 flex items-start mx-4 shadow-xl">
+    <div class="container mx-auto p-9 max-w-sm rounded-2xl overflow-hidden shadow-xl hover:shadow-2xl transition duration-300">
+      <img class="rounded-xl" src="https://images.unsplash.com/photo-1547517023-7ca0c162f816" alt="" />
+      <div class="flex justify-between items-center">
+        <div>
+          <h1 class="mt-5 mr-3 text-1xl font-semibold">Aloe Cactus</h1>
+          <p class="mt-2">$11.99</p>
+        </div>
+        <div>
+          <button class="text-white text-md font-semibold bg-yellow-500 py-2 px-4 rounded-lg shadow-md hover:shadow-lg transition duration-500 transform-gpu hover:scale-110 ">SOLICITAR</button>
+        </div>
+      </div>
+      <div>
+         <span class="text-lg">Decripcion</span>
+         <p>jnd vcwnucv vwnuo cujwqhnd vwuodjhc vnsuodhnc cv xjonwc xcvnwjdnv xijhbcq</p>
+      </div>
+    </div>
+  </div>
+  
+  <div class="min-h-full mb-8 flex items-start mx-4 shadow-xl">
+    <div class="container mx-auto p-9 max-w-sm rounded-2xl overflow-hidden shadow-xl hover:shadow-2xl transition duration-300">
+      <img class="rounded-xl" src="https://images.unsplash.com/photo-1547517023-7ca0c162f816" alt="" />
+      <div class="flex justify-between items-center">
+        <div>
+          <h1 class="mt-5 mr-3 text-1xl font-semibold">Aloe Cactus</h1>
+          <p class="mt-2">$11.99</p>
+        </div>
+        <div>
+          <button class="text-white text-md font-semibold bg-yellow-500 py-2 px-4 rounded-lg shadow-md hover:shadow-lg transition duration-500 transform-gpu hover:scale-110 ">SOLICITAR</button>
+        </div>
+      </div>
+      <div>
+         <span class="text-lg">Decripcion</span>
+         <p>jnd vcwnucv vwnuo cujwqhnd vwuodjhc vnsuodhnc cv xjonwc xcvnwjdnv xijhbcq</p>
+      </div>
+    </div>
+  </div>
+  
+  <div class="min-h-full mb-8 flex items-start mx-4 shadow-xl">
+    <div class="container mx-auto p-9 max-w-sm rounded-2xl overflow-hidden shadow-xl hover:shadow-2xl transition duration-300">
+      <img class="rounded-xl" src="https://images.unsplash.com/photo-1547517023-7ca0c162f816" alt="" />
+      <div class="flex justify-between items-center">
+        <div>
+          <h1 class="mt-5 mr-3 text-1xl font-semibold">Aloe Cactus</h1>
+          <p class="mt-2">$11.99</p>
+        </div>
+        <div>
+          <button class="text-white text-md font-semibold bg-yellow-500 py-2 px-4 rounded-lg shadow-md hover:shadow-lg transition duration-500 transform-gpu hover:scale-110 ">SOLICITAR</button>
+        </div>
+      </div>
+      <div>
+         <span class="text-lg">Decripcion</span>
+         <p>jnd vcwnucv vwnuo cujwqhnd vwuodjhc vnsuodhnc cv xjonwc xcvnwjdnv xijhbcq</p>
+      </div>
+    </div>
+  </div>
+  
+  
+  
+
+ 
+</div>
+
+
+@endsection
